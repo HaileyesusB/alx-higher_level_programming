7d7caa997165cbfd0d3084740016c1b11a798537
Haileyesus Belew <88820045+HaileyesusB@users.noreply.github.com>
2022-12-16 21:54:13 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""Request ALX SE MOdule."""
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+def main() -> None:
+    """Fetch https://alx-intranet.hbtn.io/status."""
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    content = res.text
+    ct_type = type(content)
+    output = f"Body response:\n\t- type: {ct_type}\n\t- content: {content}"
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
